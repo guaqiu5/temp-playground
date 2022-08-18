c6f9d720ad7444fd80c20287ef486998238017cf
guaqiu <6947640@qq.com>
2022-08-18 21:43:08 +0800

    feat: 还差个card

---
+++ src/components/xy-card/index.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames"
+import * as React from 'react'
+import './index.css'
+import icon1 from '../../assets/cardIcon1.png'
+interface ICard{
+    children ?: React.ReactNode;
+    className ?: any;
+    programName ?: string;
+    dayCount ?: number;
+    progress ?: string;
+    iconType ?: 0;
+}
+
+function XyCard(props:ICard){
+    const classes = classNames('xy-card',props.className)
+    return <div className={classes}>
+        <div className="xy-card-programName">Writing Program 21</div>
+        <div className="xy-card-dayCount">21 Days</div>
+        <div className="xy-card-progress">Well done!!!</div>
+        <div className="xy-card-icon">{
+            props.iconType === 0 ? 
+                <img src={icon1} alt="" /> :
+                <img src={icon1} alt="" />
+            }
+        </div>
+        <div className="xy-card-extra">{props.children}</div>
+    </div>
+}
+
+export default XyCard
--- src/pages/Host/index.tsx
+++ src/pages/Host/index.tsx
@@ -1,6 +1,7 @@
 import XyButton from "/src/components/xy-button"
 import XyNav from "/src/components/xy-nav"
 import XyImg from "/src/components/xy-img"
+import XyCard from "/src/components/xy-card"
 import './index.css'
 import _img from '../../assets/sisyphus.png'
 
@@ -10,9 +11,10 @@ function Host(){
     <XyButton className='host-button'>My Tracker</XyButton>
     <XyImg src={_img} className="host-img"/>
     <div className="host-yellow">
-        <div>Hi, Jerry!</div>
-        <div>I fell in love with you at first sight! ❤️❤️❤️</div>
+        <div className="yellow-title">Hi, Jerry!</div>
+        <div className="yellow-desc">I fell in love with you at first sight! ❤️❤️❤️</div>
     </div>
+    <XyCard className="white-1"/>
     <XyNav />
     </div>
 }
